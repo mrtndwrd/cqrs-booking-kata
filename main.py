52e1df6765d2a4ff472fbc8bcb4497dc626aae5d
Maarten de Waard <Maarten.deWaard@ns.nl>
2025-10-29 12:37:28 +0000

    free room half implemented

--- main.py
+++ main.py
@@ -1,3 +1,32 @@
+from dataclasses import dataclass
+from datetime import date
+
+
+@dataclass(eq=True, frozen=True)
+class Booking:
+    client_id: int
+    room_name: str
+    arrival_date: date
+    departure_date: date
+
+
+@dataclass(eq=True, frozen=True)
+class Room:
+    room_name: str
+
+
 class QueryService:
+    def __init__(self, rooms=[], bookings=[]):
+        self.rooms = rooms
+        self.bookings = bookings
+
+    def unbooked_room_generator(self):
+        for room in self.rooms:
+            for booking in self.bookings:
+                if booking.room_name != room.room_name:
+                    yield room
+
     def free_rooms(self, arrival_date, departure_date):
-        return set()
+        if self.bookings:
+            return set(self.unbooked_room_generator())
+        return set(self.rooms)
--- test_main.py
+++ test_main.py
@@ -1,9 +1,53 @@
 from datetime import date
-from main import QueryService
+from main import Booking, QueryService, Room
+
+arrival_date = date(2023, 1, 1)
+departure_date = date(2023, 1, 2)
 
 
 def test_free_rooms_returns_empty_set_when_there_are_no_rooms():
     query_service = QueryService()
-    result = query_service.free_rooms(arrival_date=date(
-        2023, 1, 1), departure_date=date(2023, 1, 2))
+    result = query_service.free_rooms(
+        arrival_date=arrival_date, departure_date=departure_date)
     assert result == set()
+
+
+def test_free_rooms_returns_a_room_when_there_are_no_bookings():
+    query_service = QueryService(rooms=[Room(room_name="101")], bookings=[])
+    result = query_service.free_rooms(
+        arrival_date=arrival_date, departure_date=departure_date)
+    assert result == {Room(room_name="101")}
+
+
+def test_free_rooms_returns_no_room_if_there_is_a_booking_for_it():
+    query_service = QueryService(
+        rooms=[Room(room_name="101")],
+        bookings=[Booking(client_id=1, room_name="101",
+                          arrival_date=arrival_date, departure_date=departure_date)],
+    )
+    result = query_service.free_rooms(
+        arrival_date=arrival_date, departure_date=departure_date)
+    assert result == set()
+
+
+def test_free_rooms_returns_only_the_rooms_that_have_no_bookings():
+    query_service = QueryService(
+        rooms=[Room(room_name="101"), Room(room_name="102")],
+        bookings=[Booking(client_id=1, room_name="101",
+                          arrival_date=arrival_date, departure_date=departure_date)],
+    )
+    result = query_service.free_rooms(
+        arrival_date=arrival_date, departure_date=departure_date)
+    assert result == {Room(room_name="102")}
+
+
+def test_free_rooms_returns_only_the_rooms_that_have_no_bookings():
+    query_service = QueryService(
+        rooms=[Room(room_name="101"), Room(
+            room_name="102"), Room(room_name="103")],
+        bookings=[Booking(client_id=1, room_name="101",
+                          arrival_date=arrival_date, departure_date=departure_date)],
+    )
+    result = query_service.free_rooms(
+        arrival_date=arrival_date, departure_date=departure_date)
+    assert result == {Room(room_name="102"), Room(room_name="103")}
